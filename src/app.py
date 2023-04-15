8186f65a886bfecc777190f7b599a7c66a2693f9
Alefantozzi86 <alefantozzi86@gmail.com>
2023-04-15 00:53:14 +0000

    Family_API

--- src/app.py
+++ src/app.py
@@ -26,17 +26,26 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def members_all():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_especific_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member_data = request.json
+    jackson_family.add_member(**new_member_data)
+    return "Family member added"
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    response_body = jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
